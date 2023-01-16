b5fb37bc241775b0a785fa9ac4c66657a1d430fb
imamulikhlas <imamulikhlas@gmail.com>
2023-01-16 17:24:03 +0700

    fix 404 page

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -8,15 +8,12 @@
     <title>404 NOT FOUND</title>
 </head>
 <body>
-    <div class="flex items-center justify-center h-screen">
-        <div class="relative">
-            <img src="{{ asset('assets/img/logo.png') }}">
-            <h1 class="text-4xl font-bold text-center">
-                BELUM ADA APA APA BABY KEYCHAAYANGAN IMAM ❤
-            </h1>
-            <a href="{{ route('home') }}" class="rounded-full text-primary font-bold">BALEK BALEK..</a>
+    <div class="flex justify-center h-screen">
+        <div class="flex flex-col items-center">
+          <img src="{{ asset('assets/img/logo.png') }}" alt="Your image">
+          <h1 class="text-4xl text-center">BELUM ADA APA APA BABY KEYCHAAYANGAN IMAM ❤</h1>
+          <a href="{{ route('home') }}" class="rounded-full text-primary font-bold">BALEK BALEK..</a>
         </div>
-
     </div>
 </body>
 </html>
